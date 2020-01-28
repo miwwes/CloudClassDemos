a5f6b1f9041368e6cd79712edfaa4f4d0bcbf272
miwwes <miwwes@vt.edu>
2020-01-28 12:25:40 -0500

    1/28/2020 Callback/Async Demo

--- index.ts
+++ index.ts
@@ -4,17 +4,32 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
+// We first created printstring with print all
+// then we created callback
+// lastly we created promises
 function printString(string) {
-  setTimeout(()=> {
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=> {
     console.log(string);
+    resolve();
+    // callback();
   }, Math.floor((Math.random()*100)+1));
+  });
   
 }
-
-function printAll() {
-  printString("A");
-  printString("B");
-  printString("C");
-  printString("D");
+printAll();
+// easiest way to do void functions? 
+function printAll(){
+  printString("A").then(()=>printString("B")).then(()=>printString("C")).then(()=>{printString("D")});
+  // printString("A", ()=> {
+  //   printString("B", ()=> {
+  //     printString("C", ()=> {
+  //       // void function for d 
+  //       printString("D", ()=> {});
+  //     });
+  //   });
 }
+  // only use ({}); for function within function callbacks paramater 
+  //not just a "void" parameter
+  
+  
